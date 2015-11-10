05cb11f9e85898c5d83cc575a4c86bf19898418e
PreskoIsTheGreatest <preskosemov@abv.bg>
2015-11-10 19:41:13 +0200

    Fixes

--- 01/binary_heap.js
+++ 01/binary_heap.js
@@ -1,101 +1,98 @@
-module.exports = (function (arr) {
+BinaryHeap.prototype = Object.create(Array.prototype);
 
-    BinaryHeap.prototype = Object.create(Array.prototype);
+function BinaryHeap(arr) {
+	arr.forEach(function(el){
+		this.push(el);
+		this.bubbleDown();
+	}.bind(this));
+}
 
-    function BinaryHeap(arr) {
-        arr.forEach(function(el){
-            this.push(el);
-            this.bubbleDown();
-        }.bind(this));
-    }
+BinaryHeap.prototype.swap = function (first, second) {
+	var temp = this[first];
+	this[first] = this[second];
+	this[second] = temp;
+}
 
-    BinaryHeap.prototype.swap = function (first, second) {
-        var temp = this[first];
-        this[first] = this[second];
-        this[second] = temp;
-    }
+BinaryHeap.prototype.bubbleDown = function () {
+	var lastIndex = this.length - 1;
+	var last = this[lastIndex];
+	var parentIndex = this.parentIndex(lastIndex);
+	var parent = this[parentIndex];
+	while (last < parent) {
+		this.swap(parentIndex, lastIndex);
+		lastIndex = parentIndex;
+		last = this[lastIndex];
+		parentIndex = this.parentIndex(lastIndex);
+		parent = this[parentIndex];
+	}
+}
 
-    BinaryHeap.prototype.bubbleDown = function () {
-        var lastIndex = this.length - 1;
-        var last = this[lastIndex];
-        var parentIndex = this.parentIndex(lastIndex);
-        var parent = this[parentIndex];
-        while (last < parent) {
-            this.swap(parentIndex, lastIndex);
-            lastIndex = parentIndex;
-            last = this[lastIndex];
-            parentIndex = this.parentIndex(lastIndex);
-            parent = this[parentIndex];
-        }
-    }
+BinaryHeap.prototype.parentIndex = function (searched) {
+	return Math.floor((searched - 1) / 2);
+}
 
-    BinaryHeap.prototype.parentIndex = function (searched) {
-        return Math.floor((searched - 1) / 2);
-    }
+BinaryHeap.prototype.leftIndex = function (searched) {
+	return searched * 2 + 1;
+}
 
-    BinaryHeap.prototype.leftIndex = function (searched) {
-        return searched * 2 + 1;
-    }
+BinaryHeap.prototype.rightIndex = function (searched) {
+	return searched * 2 + 2;
+}
 
-    BinaryHeap.prototype.rightIndex = function (searched) {
-        return searched * 2 + 2;
-    }
+BinaryHeap.prototype.parentElement = function (searched) {
+	return this[this.parentIndex(searched)];
+}
 
-    BinaryHeap.prototype.parentElement = function (searched) {
-        return this[this.parentIndex(searched)];
-    }
+BinaryHeap.prototype.leftElement = function (searched) {
+	return this[this.leftIndex(searched)];
+}
 
-    BinaryHeap.prototype.leftElement = function (searched) {
-        return this[this.leftIndex(searched)];
-    }
+BinaryHeap.prototype.rightElement = function (element) {
+	return this[this.rightIndex(searched)];
+}
 
-    BinaryHeap.prototype.rightElement = function (element) {
-        return this[this.rightIndex(searched)];
-    }
+BinaryHeap.prototype.isEmpty = function () {
+	return this.length === 0;
+}
 
-    BinaryHeap.prototype.isEmpty = function () {
-        return this.length === 0;
-    }
+BinaryHeap.prototype.insert = function (element) {
+	this.push(element);
+	this.bubbleDown();
+}
 
-    BinaryHeap.prototype.insert = function (element) {
-        this.push(element);
-        this.bubbleDown();
-    }
+BinaryHeap.prototype.bubbleUp = function () {
+	var first = this[0];
+	var firstIndex = 0;
+	var leftIndex = this.leftIndex(firstIndex);
+	var rightIndex = this.rightIndex(firstIndex);
+	var left = this[leftIndex];
+	var right = this[rightIndex];
+	while (firstIndex < this.length - 2 && (left || right)) {
+		if (first <= left && first <= right) {
+			break;
+		} else if (left <= right) {
+			this.swap(firstIndex, leftIndex);
+			firstIndex = leftIndex;
+		} else if (right <= left) {
+			this.swap(firstIndex, rightIndex);
+			firstIndex = rightIndex;
+		} else {
+			break;
+		}
+		first = this[firstIndex];
+		leftIndex = this.leftIndex(firstIndex);
+		rightIndex = this.rightIndex(firstIndex);
+		left = this[leftIndex];
+		right = this[rightIndex];
+	}
+}
 
-    BinaryHeap.prototype.bubbleUp = function () {
-        var first = this[0];
-        var firstIndex = 0;
-        var leftIndex = this.leftIndex(firstIndex);
-        var rightIndex = this.rightIndex(firstIndex);
-        var left = this[leftIndex];
-        var right = this[rightIndex];
-        while (firstIndex < this.length - 2 && (left || right)) {
-            if (first <= left && first <= right) {
-                break;
-            } else if (left <= right) {
-                this.swap(firstIndex, leftIndex);
-                firstIndex = leftIndex;
-            } else if (right <= left) {
-                this.swap(firstIndex, rightIndex);
-                firstIndex = rightIndex;
-            } else {
-                break;
-            }
-            first = this[firstIndex];
-            leftIndex = this.leftIndex(firstIndex);
-            rightIndex = this.rightIndex(firstIndex);
-            left = this[leftIndex];
-            right = this[rightIndex];
-        }
-    }
+BinaryHeap.prototype.getMinimum = function () {
+	this.swap(0, this.length - 1);
+	var min = this.pop();
+	this.bubbleUp();
+	this.bubbleDown();
+	return min;
+}
 
-    BinaryHeap.prototype.getMinimum = function () {
-        this.swap(0, this.length - 1);
-        var min = this.pop();
-        this.bubbleUp();
-        this.bubbleDown();
-        return min;
-    }
-
-    return BinaryHeap;
-})();
+module.exports = BinaryHeap;
--- 01/index.js
+++
@@ -1,6 +0,0 @@
-var BinaryHeap = require('./binary_heap');
-var myHeap = new BinaryHeap([4, 7, 2]);
-
-myHeap.insert(1);
-console.log(myHeap.isEmpty());    //false
-console.log(myHeap.getMinimum()); //1
